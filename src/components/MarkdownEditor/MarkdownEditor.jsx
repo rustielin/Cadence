83e125fd81631863ebafe46bf46950ca6b3ac546
Rustie (Ting-Chi) Lin <rustielin@berkeley.edu>
2020-02-15 16:27:38 -0800

    Add mde styling

--- src/components/MarkdownEditor/MarkdownEditor.jsx
+++ src/components/MarkdownEditor/MarkdownEditor.jsx
@@ -3,26 +3,60 @@ import SimpleMDE from "react-simplemde-editor";
 import "./MarkdownEditor.css";
 import "easymde/dist/easymde.min.css";
 
+// XXX: hack to disable shortcuts in simplemde
+const disabledShortcuts = {
+    toggleBold: "",
+    toggleItalic: "",
+    toggleStrikethrough: "",
+    toggleHeadingSmaller: "",
+    toggleHeadingBigger: "",
+    toggleHeading1: "",
+    toggleHeading2: "",
+    toggleHeading3: "",
+    toggleCodeBlock: "",
+    toggleBlockquote: "",
+    toggleUnorderedList: "",
+    toggleOrderedList: "",
+    cleanBlock: "",
+    drawLink: "",
+    drawImage: "",
+    drawTable: "",
+    drawHorizontalRule: "",
+    togglePreview: "",
+    toggleSideBySide: "",
+    toggleFullScreen: ""
+}
+
 class MarkdownEditor extends React.Component {
 
-    state = { mdeValue: "" };
+    constructor() {
+        super();
+        const content = localStorage.getItem("content");
+        this.state = {
+            mdeValue: content? content : ""
+        }
+    }
     
     handleChange = value => {
         this.setState({ mdeValue: value });
+        const content = value;
+		localStorage.setItem("content", content);
     };
 
 	render = () => {
 		return (
             <div className="editorContainer">
                 <SimpleMDE
-                    id="your-custom-id"
+                    className="editor"
+                    id="editor"
                     onChange={this.handleChange}
                     value={this.state.mdeValue}
                     options={{
                         toolbar: false,
                         spellChecker: false,
                         indentWithTabs: true,
-                        forceSync: true
+                        forceSync: true,
+                        shortcuts: disabledShortcuts
                     }}
                 />
             </div>
